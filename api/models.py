4e6ef1bdbafb3d9a288164ed141f3d35251832ff
Nacef LABIDI <nacef.labidi@gmail.com>
2017-09-15 14:54:34 +0100

    Add field

--- api/models.py
+++ api/models.py
@@ -5,6 +5,7 @@ class Bar(models.Model):
     address = models.TextField()
     tel = models.CharField(max_length=30)
     email = models.EmailField()
+    rating = models.IntegerField()
 
     def __str__(self):
         return self.name
