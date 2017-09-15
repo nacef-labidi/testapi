6e01cb7409df46d1c4b4802047df6b0b15d34ee1
Nacef LABIDI <nacef.labidi@gmail.com>
2017-09-15 13:40:21 +0100

    Initial commit

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Bar(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    tel = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
